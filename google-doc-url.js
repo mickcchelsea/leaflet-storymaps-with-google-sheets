5d0bb68784b4ce671ac1bed8b488dfcf6b020ab2
mickcchelsea <61745052+mickcchelsea@users.noreply.github.com>
2020-03-03 15:07:59 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/13JS5BBsNjVS1W-KUpiuvwhIaL6zgHypjUUv9yOeOV_4/edit#gid=0';
